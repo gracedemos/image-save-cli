cb839de0dd15eecabd2d98bb104e5d20e011e1fe
gracedemos <graysondemos@gmail.com>
2024-07-27 03:56:27 -0400

    First commit

---
+++ src/main.rs
@@ -0,0 +1,111 @@
+use solana_client::rpc_client::RpcClient;
+use solana_sdk::pubkey::Pubkey;
+use solana_sdk::signer::{keypair, EncodableKeypair};
+use solana_sdk::instruction::{Instruction, AccountMeta};
+use solana_sdk::transaction::Transaction;
+use solana_sdk::system_program;
+use serde::{Serialize, Deserialize};
+use std::env;
+
+const PROGRAM_ID: &str = "imgZzuUv47Wwy6aV39mAksorLYZkUfswwp74Bq9PPjX";
+const RPC_URL: &str = "http://localhost:8899";
+
+#[derive(Serialize, Deserialize)]
+struct Image {
+    url: String,
+    title: String
+}
+
+fn main() {
+    let mut args = env::args();
+    let _ = args.next().unwrap();
+    let operation = args.next().unwrap();
+
+    match operation.as_str() {
+        "add" => {
+            let keypair = args.next().unwrap();
+            let url = args.next().unwrap();
+            let title = args.next().unwrap();
+
+            let image = Image {
+                url,
+                title
+            };
+
+            add_image(keypair, image);
+        },
+        "close" => {
+            let keypair = args.next().unwrap();
+            let pda = args.next().unwrap();
+
+            close_account(keypair, pda);
+        },
+        _ => ()
+    }
+}
+
+fn add_image(keypair: String, image: Image) {
+    let program_id = Pubkey::try_from(PROGRAM_ID).unwrap();
+    let signer = keypair::read_keypair_file(keypair).unwrap();
+
+    let (pda, _bump_seed) = Pubkey::find_program_address(
+        &[signer.encodable_pubkey().as_ref(), image.title.as_bytes()],
+        &program_id
+    );
+
+    let instruction = Instruction::new_with_bincode(
+        program_id,
+        &image,
+        vec![
+            AccountMeta::new(signer.encodable_pubkey(), true),
+            AccountMeta::new(pda, false),
+            AccountMeta::new_readonly(system_program::id(), false)
+        ]
+    );
+
+    let client = RpcClient::new(RPC_URL);
+    let recent_blockhash = client.get_latest_blockhash().unwrap();
+
+    let transaction = Transaction::new_signed_with_payer(
+        &[instruction],
+        Some(&signer.encodable_pubkey()),
+        &[signer],
+        recent_blockhash
+    );
+
+    let signature = client.send_and_confirm_transaction(&transaction).unwrap();
+
+    println!("Image Added");
+    println!("Signature: {signature}");
+}
+
+fn close_account(keypair: String, pda: String) {
+    let program_id = Pubkey::try_from(PROGRAM_ID).unwrap();
+    let signer = keypair::read_keypair_file(keypair).unwrap();
+    let pda = Pubkey::try_from(pda.as_str()).unwrap();
+
+    let instruction = Instruction::new_with_bytes(
+        program_id,
+        &[],
+        vec![
+            AccountMeta::new(signer.encodable_pubkey(), true),
+            AccountMeta::new(pda, false),
+            AccountMeta::new_readonly(system_program::id(), false)
+        ]
+    );
+
+    let client = RpcClient::new(RPC_URL);
+    let recent_blockhash = client.get_latest_blockhash().unwrap();
+
+    let transaction = Transaction::new_signed_with_payer(
+        &[instruction],
+        Some(&signer.encodable_pubkey()),
+        &[signer],
+        recent_blockhash
+    );
+
+    let signature = client.send_and_confirm_transaction(&transaction).unwrap();
+
+    println!("PDA Closed");
+    println!("Signature: {signature}");
+}
